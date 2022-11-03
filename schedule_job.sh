4f43d379925f25ca8f1c9c25e062dbb3e6034ff9
Saurav Karki <saurav.karki@fusemachines.com>
2022-11-03 19:08:52 +0545

    creating cron job

---
+++ schedule_job.sh
@@ -0,0 +1,2 @@
+echo "RUNNING  SPARK JOB "
+/opt/spark/bin/spark-submit --driver-class-path /usr/lib/jvm/java-11-openjdk-amd64/lib/postgresql-42.5.0.jar /home/saurav/Documents/Fusemachines/Spark_Project/crime.py 
